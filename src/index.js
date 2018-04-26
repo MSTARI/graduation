7557e7bba4e1ae21d3029774e83ae008a4eb771d
jiaxin.li <jiaxin.li@qunar.com>
2018-04-26 10:17:54 +0800

    testing system

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import App from './pages/App';
+import 'normalize.css';
+import './index.scss';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(
+    <MuiThemeProvider>
+        <App />
+    </MuiThemeProvider>,
+    document.getElementById('root')
+);
---
+++ src/pages/login/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './index.scss';
+  
+class Login extends React.Component {
+    render() {
+        return (
+            <div>
+            </div>
+        );
+    }
+}
+
+export default Login;
