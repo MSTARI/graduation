63ef4b3539eae918dcba2f9f3d04e9a07094ee1e
jiaxin.li <jiaxin.li@qunar.com>
2018-05-25 15:56:43 +0800

    modify remote host

--- GUI/src/commons/getData.js
+++ GUI/src/commons/getData.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
+const host = 'http://47.94.15.14:8080';
+
 const getData = (url, params) => {
-    return axios.get(url, params)
+    return axios.get(host + url, params)
         .then(res => {
             if (res.status === 200) {
                 return res.data;
@@ -13,7 +15,7 @@ const getData = (url, params) => {
 }
 
 const postData = (url, data) => {
-    return axios.post(url, data)
+    return axios.post(host + url, data)
         .then(res => {
             if (res.status === 200) {
                 return res.data;
