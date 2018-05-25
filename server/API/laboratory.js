a706e4f16c166e53904d63acd655912a91e4ba4c
jiaxin.li <jiaxin.li@qunar.com>
2018-05-25 23:49:20 +0800

    delete content-type

--- server/API/laboratory.js
+++ server/API/laboratory.js
@@ -6,7 +6,6 @@ const url = 'mongodb://localhost:27017/';
 
 router.all('*', function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Content-Type", "application/json;charset=utf-8");
     next();
 });
 
