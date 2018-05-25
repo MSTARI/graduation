7c4ec3536c279155480df0c7901a13a572b7f9f7
jiaxin.li <jiaxin.li@qunar.com>
2018-05-25 22:20:00 +0800

    server fix

--- server/API/personInfo.js
+++ server/API/personInfo.js
@@ -7,7 +7,7 @@ const url = 'mongodb://localhost:27017/';
 router.all('*', function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "X-Requested-With");
-    res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
+    res.header("Access-Control-Allow-Methods","GET,POST");
     res.header("Content-Type", "application/json;charset=utf-8");
     next();
 });
